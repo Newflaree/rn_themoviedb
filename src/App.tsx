869477a2311175dba848eb6a29018512f403b4e1
Camilo López <camiloj.lopez79@gmail.com>
2024-06-04 15:45:23 -0400

    Navegación entre pantallas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import 'react-native-gesture-handler';
 // React Native
 import {
   Text,
   View
 } from 'react-native';
+// React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+// Routes
+import { StackNavigator } from './presentation/navigation';
 
 export const App = () => {
   return (
-    <View>
-      <Text>MovieApp</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
